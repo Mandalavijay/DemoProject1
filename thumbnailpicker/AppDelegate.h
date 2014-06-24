48440d1f35b20faf46f825da24f00ba811087eea
Vijay Kumar <Vijay@Akhila.local>
2014-06-24 06:38:55 -0400

    Initial Commit

---
+++ thumbnailpicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  thumbnailpicker
+//
+//  Created by Vijay Kumar on 6/24/14.
+//  Copyright (c) 2014 Scroll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ thumbnailpicker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  thumbnailpicker
+//
+//  Created by Vijay Kumar on 6/24/14.
+//  Copyright (c) 2014 Scroll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
